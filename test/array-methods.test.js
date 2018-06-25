61552275d3855d08f7f769fe83559a89274803b2
Mark Myers <markalope@gmail.com>
2018-06-25 14:50:37 -0700

    first attenmpt at writing first test for map

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -3,9 +3,10 @@ const { map } = require('../lib/array-methods');
 
 describe('array methods', () => {
     describe('map', () => {
-
-
-
+        const originalArr = [1, 3, 5, 7];
+        //returned array = original array
+        const returnedArr = [];
+        assert.deepEqual(returnedArr, [1, 3, 5, 7]);
     });
 
 });
