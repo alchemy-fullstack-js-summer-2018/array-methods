e789d5019db934ec3e4cdfcd3871c0f51c898ce9
rbtprograms <rbt61690@gmail.com>
2018-06-25 15:36:29 -0700

    map done

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -4,20 +4,19 @@ const { map } = require('../lib/array-methods');
 describe('array methods', () => {
 
     describe('map', () => {
-        
-        describe('assumptions', () => {
-
-            it('return an new array the same length as given', () => {
-                const numbers = [1, 2, 3, 5];
-                const result = numbers.map(n => n);
-                assert.equal(result.length, numbers.length);
-            });
-        });
+        let testers = [1, 3, 5, 6];
 
         it('returns a new array the same length as given array', () => {
-            const tester = [1, 3, 5, 6];
-            const result = map(tester);
-            assert.equal(result.length, tester.length);
+            const numbers = testers;
+            const result = map(numbers, n => n);
+            assert.equal(result.length, numbers.length);
         });
+
+        it('returns a new array with changed values', () => {
+            const numbers = testers;
+            const result = map(numbers, n => (n + 1));
+            assert.deepEqual(result, [2, 4, 6, 7]);
+        });
+        
     });
 });
