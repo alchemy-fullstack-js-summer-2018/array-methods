44568e1bcad3ffd3ad27ed603c56615508f579ce
Mario Quintana <pyrros2000@gmail.com>
2018-06-25 15:53:31 -0700

    returning correct new array values but getting assertion error

--- test/array.test.js
+++ test/array.test.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
-const arrayMethods = require('../lib/arrays');
-const map = arrayMethods.map;
+const { map } = require('../lib/arrays');
+
 
 describe('array methods', () => {
     it('creates new array', () => {
-        const array =[2, 4, 6]; 
-        const newArray = map(array);
+        const array = [2, 4, 6]; 
+        const newArray = array.map;
         assert.equal(newArray, [3, 5, 7]);
     });
 });
