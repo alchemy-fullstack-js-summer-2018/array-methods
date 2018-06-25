4a8ce6e324c10315d69dfbb8a81422a42844d860
Mario Quintana <pyrros2000@gmail.com>
2018-06-25 15:21:35 -0700

    working on map testing

--- test/array.test.js
+++ test/array.test.js
@@ -2,10 +2,10 @@ const assert = require('assert');
 const arrayMethods = require('../lib/arrays');
 const map = arrayMethods.map;
 
-describe('arrays', () => {
-    it('creates new array with previous items', () => {
-        const array1 =[2,4,6,8]; 
-        const newArray = [];
-        assert.equal(newArray.push(array1));
+describe('array methods', () => {
+    it('creates new array', () => {
+        const array =[2, 4, 6]; 
+        const newArray = map(array);
+        assert.equal(newArray, [3, 5, 7]);
     });
 });
