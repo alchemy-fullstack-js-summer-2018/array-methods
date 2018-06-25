b4fe671bd224e3700009a6b2112efc11409435fa
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-06-25 16:33:45 -0700

    Make map function and test

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { map } = require('../lib/array-methods');
 
 describe('array methods', () => {
     describe('map', () => {
@@ -8,6 +9,12 @@ describe('array methods', () => {
                 const squared = numbers.map(n => n * n);
                 assert.deepEqual(squared, [4, 16, 36]);
             });
+        });
+
+        it('takes callback and returns new array with return value of each callback', () => {
+            const numbers = [2, 4, 6];
+            const squared = map(numbers, (n) => n * n);
+            assert.deepEqual(squared, [4, 16, 36]);
         });
     });
 });
