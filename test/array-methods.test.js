6fcc2fbed1ccb6b4360edde3eaca366d8da77d3e
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-25 16:22:32 -0700

    Passing Map Test

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -3,13 +3,13 @@ const { map } = require('../lib/array-methods');
 
 describe('array methods', () => {
 
-    describe('map', () => {
-        it('it creates a new array', () => {
-            const arr1 = [1,2,3];
-            const arr2 = map(arr1, (item) => {
-                return item + 1;
-            });
-            assert.equal(arr2, [2,3,4]);     
+    it('Map', () => {
+        const arr1 = [1,2,3];
+        const arr2 = map(arr1, (item) => {
+            return item + 1;
         });
+        assert.deepEqual(arr2, [2,3,4]);     
     });
+
+    
 });
