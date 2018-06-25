c33b0893e7295253b1f0ef3497dcc15d85dbcabb
rbtprograms <rbt61690@gmail.com>
2018-06-25 16:40:43 -0700

    findIndex done

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
-const { map, filter } = require('../lib/array-methods');
+const { map, filter, findIndex } = require('../lib/array-methods');
 
 describe('array methods', () => {
+    let testers = [1, 3, 5, 6];
 
     describe('map', () => {
-        let testers = [1, 3, 5, 6];
 
         it('returns a new array the same length as given array', () => {
             const numbers = testers;
@@ -17,22 +17,20 @@ describe('array methods', () => {
             const result = map(numbers, n => (n * n));
             assert.deepEqual(result, [1, 9, 25, 36]);
         });
-
     });
 
     describe('filter', () => {
-        let numTesters = [3, 4, 5, 6];
 
         it('returns a new array with odd values removed', () => {
-            const numbers = numTesters;
+            const numbers = testers;
             const result = filter(numbers, n => (n % 2 === 0));
-            assert.deepEqual(result, [4, 6]);
+            assert.deepEqual(result, [6]);
         });
 
         it('returns a new array with even numbers removed', () => {
-            const numbers = numTesters;
+            const numbers = testers;
             const result = filter(numbers, n => (n % 2 === 1));
-            assert.deepEqual(result, [3, 5]);
+            assert.deepEqual(result, [1, 3, 5]);
         });
 
         it('returns a new array with words over 5 letters in length gone', () => {
@@ -42,9 +40,18 @@ describe('array methods', () => {
         }); 
     });
 
-    describe('findIndex' () => {
+    describe('findIndex', () => {
 
-
-        it('returns index of the item I have given it')
-    })
+        it('returns index of the first item that meets my specs', () => {
+            const numbers = testers;
+            const result = findIndex(numbers, n => (n > 4));
+            assert.equal(result, 2);
+        });
+        
+        it('returns -1 if nothing meets my specs', () => {
+            const numbers = testers;
+            const result = findIndex(numbers, n => (n > 10));
+            assert.equal(result, -1);
+        });
+    });
 });
