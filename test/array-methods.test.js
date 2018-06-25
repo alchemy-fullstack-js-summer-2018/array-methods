8d2e0ec92b69487ab0bf16be4ee599e6a90818dd
Mark Myers <markalope@gmail.com>
2018-06-25 15:44:28 -0700

    got a working test and function for the map method

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 const { map } = require('../lib/array-methods');
 
 describe('array methods', () => {
-    describe('map', () => {
+    it('map', () => {
         const arr = [1, 3, 5, 7];
         //returned array = original array
-        const returnedArr = [];
-        assert.deepEqual(returnedArr, [1, 3, 5, 7]);
+        let returnedArr = map(arr, (item) => {return item;});
+        assert.deepEqual(returnedArr, arr);
     });
 
 });
