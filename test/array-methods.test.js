b508b9d60b97ba5ab96c589fe8de57fcc9da3d3d
Mark Myers <markalope@gmail.com>
2018-06-25 16:41:38 -0700

    wrote initial test for every

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,12 +1,19 @@
 const assert = require('assert');
-const { map } = require('../lib/array-methods');
+const { map every } = require('../lib/array-methods');
 
-describe('array methods', () => {
-    it('map', () => {
+describe('map', () => {
+    it('new array matches original array', () => {
         const arr = [1, 3, 5, 7];
         //returned array = original array
         let returnedArr = map(arr, (item) => {return item;});
         assert.deepEqual(returnedArr, arr);
     });
+});
 
+describe('every', () => {
+    it('every item is included', () => {
+        const arr = [50, 62, 185, 70];
+        let result = (arr, (item) => {item > 49;});
+        assert.equal(result, true);      
+    });
 });
