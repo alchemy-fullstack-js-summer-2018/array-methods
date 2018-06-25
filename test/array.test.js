293ce41a300ab0681b59273dd27ac05052c24e2f
Mario Quintana <pyrros2000@gmail.com>
2018-06-25 16:39:20 -0700

    wrote filter test, got approved now starting filter function

--- test/array.test.js
+++ test/array.test.js
@@ -8,4 +8,9 @@ describe('array methods', () => {
         const newArray = map(array, (item) => { return item + 1;});
         assert.deepEqual(newArray, [3, 5, 7]);
     });
+    it('Filter: Selecting specific items from an array', () => {
+        const numbers = [1, 4, 8];
+        const evenNumbers = filter(numbers, (item) => {return item >3 })
+        assert.equal(evenNumbers, [4, 8]);
+    });
 });
