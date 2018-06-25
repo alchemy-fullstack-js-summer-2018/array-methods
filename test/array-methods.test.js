2e62d28dd9a865f58d9329880b2d323217d85990
MariahAdams <mariah.adams23@yahoo.com>
2018-06-25 16:37:52 -0700

    filter test is passing

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,15 +1,25 @@
 const assert = require('assert');
-const { map } = require('../lib/array-methods');
+const { map, filter } = require('../lib/array-methods');
 
 describe('array methods', () => {
+    const numbers = [1, 2, 3];
 
     it('returns array of the same length as given', () => {
-        const numbers = [1, 2, 3];
-        const newNum = map(numbers);
+        numbers;
+        const newNum = map(numbers, n => n);
         assert.equal(newNum.length, numbers.length);
     });
 
     it('returns a new array with the return value of each callback', () => {
-        
+        numbers;
+        const newNum = map(numbers, n => (n + 1));
+
+        assert.deepEqual(newNum, [2, 3, 4]);
+    });
+
+    it('returns new arr with items whose callback returns true', () => {
+        numbers;
+        const filtered = filter(numbers, n => n >= 2);
+        assert.deepEqual(filtered, [2, 3]);
     });
 });
