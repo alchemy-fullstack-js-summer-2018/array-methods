40163b644c5409a6453b69f596614e7f69bda601
Robyn Navarro <rnavarro182@gmail.com>
2018-06-26 11:35:52 -0700

    finding index test passed

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
-const { mapping } = require('../lib/array-methods');
+const { mapping, filtering, findingIndex } = require('../lib/array-methods');
 
 describe('array methods', () => {
+    
+    const numbers = [0, 1, , 2, 3, 4, 5];
+    const strings = ['a', 'b', 'c',,, 'e'];
 
     describe('mapping', () => {
         
@@ -19,12 +22,49 @@ describe('array methods', () => {
         describe('assumption', () => {
 
             it('filters numbers > 2', () => {
-                const a = [1, 2, 3, 4];
-                let na = a.filter(el => el > 2);
-                assert.deepEqual(na, [3, 4]);
+                let na = numbers.filter(el => el > 2);
+                assert.deepEqual(na, [3, 4, 5]);
             });
         });
 
+        describe('filtering test', () => {
+
+            it('returns odd numbers', () => { 
+                let na = filtering(numbers, el => el % 2 === 1);
+                assert.deepEqual(na, [1, 3, 5]);
+            });
+
+            it('returns even numbers', () => { 
+                let na = filtering(numbers, el => el % 2 === 0);
+                assert.deepEqual(na, [0, 2, 4]);
+            });
+        });
+    });
+    describe('finding index', () => {
+
+        describe('assumption', () => {
+
+            it('returns index 0', () => {
+                let index = strings.indexOf('a');
+                assert.equal(index, 0);
+            });
+        });
+        describe('finding index test', () => {
+
+            it('returns position of "e"', () => {
+                let index = findingIndex(strings, (el) => {
+                    return el === 'e';
+                });
+                assert.equal(index, 3);
+            });
+        });
+    });
+    describe('reducing', () => {
+
+        describe('assumption', () => {
+
+            it('returns sum of numbers in array')
+        });
     });
 
 });
