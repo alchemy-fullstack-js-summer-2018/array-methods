6041503b38cad6a3c076f87376d8b3f99f2498ad
Rehmsy <sarahrehmer@gmail.com>
2018-06-25 21:32:29 -0700

    added another test for if nothing found, then got functions to pass tests.

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -21,16 +21,19 @@ describe('array methods', () => {
         it('value higher than 2', () => {
             const numbers = [1, 2, 3];
             const result = myFilter(numbers, x => x > 2);
-            assert.deepEqual(result, [3]);
-            
+            assert.deepEqual(result, [3]);    
         });
     });
-    
     describe('find index', () => {
         it('find index of 2 which is 1', () => {
             const numbers = [1, 2, 3];
             const result = myFindIndex(numbers, x => x === 2);
             assert.equal(result, 1);
+        });
+        it('return -1 if nothing found', () => {
+            const numbers = [1, 2, 3];
+            const result = myFindIndex(numbers, x => x > 3);
+            assert.deepEqual(result, -1);
         });
     });
 });
