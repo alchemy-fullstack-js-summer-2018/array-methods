97682049c1b461baa0388b108995435a13499dea
Mario Quintana <pyrros2000@gmail.com>
2018-06-25 17:44:13 -0700

    completed filter test and function, but cannot use .push only given array functions

--- test/array.test.js
+++ test/array.test.js
@@ -8,9 +8,12 @@ describe('array methods', () => {
         const newArray = map(array, (item) => { return item + 1;});
         assert.deepEqual(newArray, [3, 5, 7]);
     });
+
     it('Filter: Selecting specific items from an array', () => {
         const numbers = [1, 4, 8];
-        const newNumbers = filter(numbers, (item) => {return item >3 })
+        const newNumbers = filter(numbers, (item) => {return item > 3;})
         assert.deepEqual(newNumbers, [4, 8]);
     });
+
+
 });
