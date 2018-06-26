d1b06927173ce1886686e46beec269be654da481
Mark Myers <markalope@gmail.com>
2018-06-25 21:56:08 -0700

    evening work

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { map, every } = require('../lib/array-methods');
+const { map, every, filter } = require('../lib/array-methods');
 
 describe('map', () => {
     it('new array matches original array', () => {
@@ -15,5 +15,27 @@ describe('every', () => {
         const arr = [50, 63, 185, 70, 99];
         let result = every(arr, (item) => {return item > 49;});
         assert.deepEqual(result, [50, 63, 185, 70, 99]);      
+    });
+});
+
+// describe('index', () => {
+//     it('return index of string', () => {
+//         const arr = [50, 100, 185, 70, 99];
+//         let result = index(arr, (indexItem) => {return indexItem > 150;});
+//         assert.equal(result, [50, 100, 185, 70, 99]);      
+//     });
+// });
+
+const isEven = x => x % 2 === 0;
+
+describe('filter', () => {
+    it('filter out odd values', () => {
+        const numbers = [50, 63, 185, 70, 99];
+        const even = [];
+        numbers.filter(n => {
+            even.push.isEven(n);
+            return isEven(n);
+        });
+        assert.equal(even, true);      
     });
 });
