8d2e5a945c3105f0a7396781859f88c40537a31b
Easton Gorishek <ieaston26@gmail.com>
2018-06-25 17:44:40 -0700

    filter and findIndex done

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { map } = require('../lib/array-methods');
+const { map, filter, findIndex } = require('../lib/array-methods');
 
 describe ('array methods', () => {
 
@@ -15,8 +15,53 @@ describe ('array methods', () => {
 
         it('returns each value in each array position by one', () => {
             const numbers = [3, 4, 5];
-            const result = map(numbers, n => n + 1);
+            const result = map(numbers, i => i + 1);
             assert.deepEqual(result, [4, 5, 6]);
         });
+    });
+
+    describe('filter', () => {
+
+        describe('assumptions', () => {
+            it('returns only even numbers within an array', () => {
+                const numbers = [2, 4, 5, 7, 8];
+                const result = numbers.filter(i => i % 2 === 0);
+                assert.deepEqual(result, [2, 4, 8]);
+            });
+        });
+
+        it('returns only even numbers within an array', () => {
+            const numbers = [2, 4, 5, 7, 8];
+            const result = filter(numbers, i => i % 2 === 0);
+            assert.deepEqual(result, [2, 4, 8]);
+        });
+    });
+
+    describe('findIndex', () => {
+
+        describe('assumptions', () => {
+            it('returns first index position where value is > than 25', () => {
+                const numbers = [2, 25, 70, 1];
+                const result = numbers.findIndex(i => i > 25);
+                assert.equal(result, 2);
+            });
+        });
+
+        it('returns first index position where value is > than 25', () => {
+            const numbers = [2, 25, 70, 1];
+            const result = findIndex(numbers, i => i > 25);
+            assert.equal(result, 2);
+        });
+
+        it('returns false if no matches found', () => {
+            const numbers = [2, 25, 10, 1];
+            const result = findIndex(numbers, i => i > 25);
+            assert.equal(result, false);
+        });
+    });
+
+    describe('reduce', () => {
+        
+        describe('assumptions', () => {})
     });
 });
