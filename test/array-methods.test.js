98881f78d18443b5f108091735226f3b6ba992b0
rbtprograms <rbt61690@gmail.com>
2018-06-25 17:33:05 -0700

    reduce refactored

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,5 +1,9 @@
 const assert = require('assert');
-const { map, filter, findIndex, reduce } = require('../lib/array-methods');
+const { map, 
+    filter, 
+    findIndex, 
+    reduce, 
+    every } = require('../lib/array-methods');
 
 describe('array methods', () => {
     let testers = [1, 3, 5, 6];
@@ -59,13 +63,28 @@ describe('array methods', () => {
 
         it('returns the sum of all the numbers in an array', () => {
             const numbers = testers;
-            const result = reduce(numbers, n => (n), 0);
+            const result = reduce(numbers, (acc, item) => (acc + item), 0);
             assert.equal(result, 15);
         });
         it('returns the sum of all the numbers in an array, bigger', () => {
             const numbers = [10, 20, 30];
-            const result = reduce(numbers, n => (n), 0);
+            const result = reduce(numbers, (acc, item) => (acc + item), 0);
             assert.equal(result, 60);
+        });
+    });
+
+    describe('every', () => {
+
+        it('returns true if all the items are less than 10', () => {
+            const numbers = testers;
+            const result = every(numbers, n => (n < 10));
+            assert.equal(result, true);
+        });
+
+        it('returns false if anything is greater than 4', () => {
+            const numbers = testers;
+            const result = every(numbers, n => (n > 4));
+            assert.equal(result, false);
         });
     });
 });
